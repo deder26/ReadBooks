a371603aa5114156505ef4ff737ce1ebc989b543
deder26 <30472231+deder26@users.noreply.github.com>
2019-08-22 17:56:43 +0600

    Add files via upload

---
+++ resources/views/front-end/books/SearchNotFound.blade.php
@@ -0,0 +1,27 @@
+@extends('front-end.master') 
+
+@section('title')
+<title>Search Results</title>
+@endsection
+
+@section('content')
+	<div class="col-md-9 top-right">
+	<div class="welcome">
+		<h2>
+			<span>Sorry, Search Doesn't Exist</span>
+		</h2>
+		<div class="welcome-top">
+			<div class="col-md-6 welcome-left">
+				<div class="view view-tenth">
+					<a href="singlepage.html">
+						<div class="inner_content clearfix">
+						
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</div>
+@endsection
